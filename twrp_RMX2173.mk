4f25245e3dc4c993f419073ba5c5a94bdcd4c1b7
sundaqiang <393116105@qq.com>
2021-09-28 18:17:57 +0800

    third commit

--- twrp_RMX2173.mk
+++ twrp_RMX2173.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2021 TeamWin Recovery Project
+# Copyright (C) 2021 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,31 +14,38 @@
 # limitations under the License.
 #
 
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from TWRP common configurations
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
-
-# Extra required packages
-PRODUCT_PACKAGES += \
-    libion.recovery \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
-    fastbootd
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+	$(LOCAL_PATH)/prebuilt/dtb:dtb.img
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX2173
 PRODUCT_NAME := twrp_RMX2173
-PRODUCT_BRAND := realme
-PRODUCT_MODEL := RMX2173
-PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := RealMe
+PRODUCT_MODEL := Q2 Pro 5G
+PRODUCT_MANUFACTURER := RealMe
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=RMX2173 \
+    PRODUCT_NAME=RMX2173
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
+    ro.vendor.build.security_patch=2099-12-31 \
+    ro.bootimage.build.date.utc=0 \
+    ro.build.date.utc=0
