a0074290b28bca9cd3d86c41c958e845ec0d9b32
sundaqiang <393116105@qq.com>
2021-10-19 17:01:31 +0800

    Adjust the script to modify the model

--- recovery/root/system/bin/modelchange.sh
+++ recovery/root/system/bin/modelchange.sh
@@ -1,50 +1,68 @@
 #!/system/bin/sh
-product=$(sed -n '/ro.build.product/ p' /cache/runtime.prop | cut -d'=' -f2)
-device=$(sed -n '/ro.commonsoft.ota/ p' /cache/runtime.prop | cut -d'=' -f2)
-soft=$(sed -n '/ro.separate.soft/ p' /cache/runtime.prop | cut -d'=' -f2)
-name=$(sed -n '/ro.oppo.market.name/ p' /cache/runtime.prop | cut -d'=' -f2 | sed 's/realme //')
-echo "product is ${product}"
-echo "device is ${device}"
-echo "soft is ${soft}"
-echo "name is ${name}"
-
-if [ "$product" ]; then
-  echo "change product"
-  sed -i "s/RMX2173/${product}/" /prop.default
-  sed -i "s/RMX2173/${product}/" /default.prop
-  resetprop ro.product.name "omni_${product}"
-  resetprop ro.product.odm.name "omni_${product}"
-  resetprop ro.product.vendor.name "omni_${product}"
-fi
-
-if [ "$device" ]; then
-  echo "change device"
-  resetprop ro.product.device "${device}"
-  resetprop ro.product.odm.device "${device}"
-  resetprop ro.product.vendor.device "${device}"
+grep "ro.product.device=" /prop.default >/dev/null
+if [ $? -eq 0 ]; then
+  echo "change done"
 else
-  echo "pass change device"
-  resetprop ro.product.device RMX2175CN
-  resetprop ro.product.odm.device RMX2175CN
-  resetprop ro.product.vendor.device RMX2175CN
+  i=1
+  while [ $i -le 10 ]; do
+    if [ -f /cache/runtime.prop ]; then
+      product=$(sed -n '/ro.build.product/ p' /cache/runtime.prop | cut -d'=' -f2)
+      device=$(sed -n '/ro.commonsoft.ota/ p' /cache/runtime.prop | cut -d'=' -f2)
+      soft=$(sed -n '/ro.separate.soft/ p' /cache/runtime.prop | cut -d'=' -f2)
+      name=$(sed -n '/ro.oppo.market.name/ p' /cache/runtime.prop | cut -d'=' -f2 | sed 's/realme //')
+      
+      echo "product is ${product}"
+      echo "device is ${device}"
+      echo "soft is ${soft}"
+      echo "name is ${name}"
+      
+      if [ "$device" ]; then
+        echo "change device"
+        sed -i "s/=RMX2173/=${device}/" /prop.default
+        sed -i "s/=RMX2173/=${device}/" /default.prop
+        sed -i "$ a ro.product.device=${device}" /prop.default
+      else
+        echo "pass change device"
+        sed -i "s/=RMX2173/=RMX2175CN/" /prop.default
+        sed -i "s/=RMX2173/=RMX2175CN/" /default.prop
+        sed -i "$ a ro.product.device=RMX2175CN" /prop.default
+      fi
+      
+      if [ "$product" ]; then
+        echo "change product"
+        sed -i "s/RMX2173/${product}/" /prop.default
+        sed -i "s/RMX2173/${product}/" /default.prop
+        sed -i "$ a ro.product.name=omni_${product}" /prop.default
+      else
+        echo "pass change product"
+        sed -i "$ a ro.product.name=omni_RMX2173" /prop.default
+      fi
+      
+      if [ "$soft" ]; then
+        echo "change soft"
+        sed -i "$ a ro.separate.soft=${soft}" /prop.default
+      else
+        echo "pass change soft"
+        sed -i "$ a ro.separate.soft=20633" /prop.default
+      fi
+      
+      if [ "$name" ]; then
+        echo "change name"
+        sed -i "s/Q2 Pro 5G/${name}/" /prop.default
+        sed -i "s/Q2 Pro 5G/${name}/" /default.prop
+        sed -i "$ a ro.product.model=${name}" /prop.default
+      else
+        echo "pass change name"
+        sed -i "$ a ro.product.model=Q2 Pro 5G" /prop.default
+      fi
+      
+      resetprop --file /prop.default
+      resetprop --file /default.prop
+      break
+    else
+      sleep 1
+      let i++
+    fi
+  done
 fi
-
-if [ "$soft" ]; then
-  echo "change soft"
-  resetprop ro.separate.soft "${soft}"
-else
-  echo "pass change soft"
-  resetprop ro.separate.soft 20633
-fi
-
-if [ "$name" ]; then
-  echo "change name"
-  sed -i "s/Q2 Pro 5G/${name}/" /prop.default
-  sed -i "s/Q2 Pro 5G/${name}/" /default.prop
-  resetprop ro.product.model "${name}"
-  resetprop ro.product.odm.model "${name}"
-  resetprop ro.product.vendor.model "${name}"
-fi
-
-resetprop --file /prop.default
-resetprop --file /default.prop
+stop modelchange
